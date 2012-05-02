2acef44e8b7b5789295b2ce43a10dc425d6f9bca
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-02 14:56:33 -0400

    First cut at rack server

--- lectures/03-rack/3.1-rack-basics-exercises/server.rb
+++ lectures/03-rack/3.1-rack-basics-exercises/server.rb
@@ -3,8 +3,24 @@ require 'rack'
 
 class WebServer
   def call(env)
-    # TODO: replace this with an implementation
-    [200, {"Content-Type" => "text/html"}, ["Hello Rack!"]]
+    top = File.join(Dir.pwd, "/public")
+    begin
+      path = env["PATH_INFO"]
+      file_path = File.join(top, Rack::Utils.unescape(path))
+      raise SecurityError unless file_path.include?(top)
+      file_path = File.join(top, "index.html") if path == '/'
+      content = File.read(file_path)
+      [200, {"Content-type" => "text/html", "Content-length" => content.size.to_s }, [content] ]
+    rescue SecurityError
+      content = File.read("public/403.html")
+      [403, {"Content-type" => "text/html", "Content-length" => content.size.to_s }, [content] ]
+    rescue Errno::ENOENT
+      content = File.read("public/404.html")
+      [404, {"Content-type" => "text/html", "Content-length" => content.size.to_s }, [content] ]
+    rescue Exception => e
+      content = File.read("public/500.html")
+      [500, {"Content-type" => "text/html", "Content-length" => content.size.to_s }, [File.join(e.message, content)] ]
+    end
   end
 end
 
