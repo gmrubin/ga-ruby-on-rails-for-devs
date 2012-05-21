e917f894af1f371634b855dc951576bce9546eb8
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-21 11:25:50 -0400

    Adds 7.1 exercises

--- lectures/07-active-record/7.1-active-record-basics-exercises/spec/fabricators/employee_fabricator.rb
+++ lectures/07-active-record/7.1-active-record-basics-exercises/spec/fabricators/employee_fabricator.rb
@@ -1,4 +1,10 @@
 Fabricator(:employee) do
   name { Faker::Name.name }
-  # TODO: populate other values
+  address { Faker::Address.street_address }
+  start_date { Date.today }
+end
+
+Fabricator(:role) do
+  name { Faker::Name.name }
+  department { Faker::Lorem.words(num = 1)}
 end
