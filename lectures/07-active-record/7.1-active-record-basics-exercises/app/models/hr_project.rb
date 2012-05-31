027435e94e433a17e1be3972aabc974eef976ceb
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-31 18:30:36 -0400

    Adds has_many :through and STI exercises

---
+++ lectures/07-active-record/7.1-active-record-basics-exercises/app/models/hr_project.rb
@@ -0,0 +1,5 @@
+class HrProject < Project
+  def description
+    "Human resources projects here."
+  end
+end
---
+++ lectures/07-active-record/7.1-active-record-basics-exercises/spec/models/employee_project_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe EmployeeProject do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
