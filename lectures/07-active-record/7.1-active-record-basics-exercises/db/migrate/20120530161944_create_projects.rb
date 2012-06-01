aafceb5416ff3247c4b731d85df9c7287ba8a737
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-01 14:25:39 -0400

    Adds minor fixes
    Places STI models in subdir, sets employees_count to default to 0

--- lectures/07-active-record/7.1-active-record-basics-exercises/db/migrate/20120530161944_create_projects.rb
+++ lectures/07-active-record/7.1-active-record-basics-exercises/db/migrate/20120530161944_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
-      t.integer :employees_count
+      t.integer :employees_count, :default => 0
 
       t.timestamps
     end
--- lectures/07-active-record/7.1-active-record-basics-exercises/db/schema.rb
+++ lectures/07-active-record/7.1-active-record-basics-exercises/db/schema.rb
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(:version => 20120531212849) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.integer  "employees_count"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.integer  "employees_count", :default => 0
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.string   "type"
   end
 
