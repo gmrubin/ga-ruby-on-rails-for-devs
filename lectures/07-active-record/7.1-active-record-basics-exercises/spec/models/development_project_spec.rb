5d7617ddf61c3744fe8f9f177089e2dd464641db
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-05 09:16:37 -0400

    changes callback to custom validation

--- lectures/07-active-record/7.1-active-record-basics-exercises/spec/models/development_project_spec.rb
+++ lectures/07-active-record/7.1-active-record-basics-exercises/spec/models/development_project_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe DevelopmentProject do
   context "Description" do
     it "Should return a hardcoded description" do
-      d = DevelopmentProject.create(name: "A development project", type: "DevelopmentProject", employees_count: 0)
+      d = DevelopmentProject.create(name: "A development project", type: "DevelopmentProject")
+      d.employees_count = 0
+      d.save!
       d.description.should == "Development projects go here."
     end
   end
