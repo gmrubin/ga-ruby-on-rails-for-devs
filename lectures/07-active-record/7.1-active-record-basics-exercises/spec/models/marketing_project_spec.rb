20204ef406caffadaebc3cda8bf49acf3d8ce129
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-06 07:26:44 -0400

    Adds Project:: to project STI and tests, moved project.rb up a dir

--- lectures/07-active-record/7.1-active-record-basics-exercises/spec/models/marketing_project_spec.rb
+++ lectures/07-active-record/7.1-active-record-basics-exercises/spec/models/marketing_project_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe MarketingProject do
+describe Project::MarketingProject do
   context "Description" do
     it "Should print out a hardcoded description" do
-      m = MarketingProject.create(name:"A marketing project", type: "MarketingProject")
+      m = Project::MarketingProject.create(name:"A marketing project", type: "MarketingProject")
       m.employees_count = 0
       m.save!
       m.description.should == "Marketing projects go here."
